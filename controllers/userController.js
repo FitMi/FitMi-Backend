65ac7d8ab3e2226a0f065260f08122fbdf92b754
Bohan Huang <weepineapple@gmail.com>
2016-10-11 02:55:25 +0800

    update user model and controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,10 @@
-// Load required packages
-var User = require('../models/user');
+var jwt = require('jsonwebtoken');
+var config = require('../config/config');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
-// Create endpoint /api/users for POST
-exports.postUsers = function(req, res) {
-  var user = new User({
-    username: req.body.username,
-    password: req.body.password
+function resError(res, err) {
+  return res.status(500).json({
+    message: err.message
   });
-
-  user.save(function(err) {
-    if (err)
-      res.send(err);
-
-    res.json({ message: 'User posted' });
-  });
-};
-
-// Create endpoint /api/users for GET
-exports.getUsers = function(req, res) {
-  User.find(function(err, users) {
-    if (err)
-      res.send(err);
-
-    res.json(users);
-  });
-};
+}
--- models/user.js
+++ models/user.js
@@ -1,15 +1,25 @@
 'use strict';
-
-// Module dependencies
+var jwt = require('jsonwebtoken');
+var config = require('../config/config');
 var mongoose = require('mongoose');
 
 // User Schema
 var UserSchema = new mongoose.Schema({
   name: { type: String, default: '' },
-  username: { type: String, default: '' },
-  provider: { type: String, default: '' },
-  facebook: {},
-  wechat: {}
+  facebookId: { type: String, default: '' },
+  wechatId: { type: String, default: '' }
 });
 
-mongoose.model('User', UserSchema);
+UserSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    name: this.name,
+    fbId: this.facebookId,
+    exp: parseInt(expiry.getTime() / 1000)
+  }, config.secret);
+};
+
+module.exports = mongoose.model('User', UserSchema);
