237f4d1f36132d504094d9f99c00c0686f11b6ea
Bohan Huang <weepineapple@gmail.com>
2016-10-11 00:59:31 +0800

    update config

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  db_uri: process.env.DB_URI || 'mongodb://localhost:27017/FitMe',
-  db_options: {
-    user: process.env.DB_USER || '',
-    pass: process.env.DB_PWD || ''
+	'app_secret': process.env.APP_SECRET,
+  'db_uri': process.env.DB_URI || 'mongodb://localhost:27017/FitMe',
+  'db_options': {
+    'user': process.env.DB_USER || '',
+    'pass': process.env.DB_PWD || ''
   },
-  facebook_clientid: process.env.FACEBOOK_CLIENTID,
-  facebook_secret: process.env.FACEBOOK_SECRET
+  'facebook_clientid': process.env.FACEBOOK_CLIENTID,
+  'facebook_secret': process.env.FACEBOOK_SECRET
 };
