fd74bc5479f9134cda02cb4389f0ca504b18f29b
Bohan Huang <weepineapple@gmail.com>
2016-10-19 02:28:03 +0800

    add getUser with user ID method

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,63 @@ var config = require('../config/config');
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
-function resError(res, err) {
-  return res.status(500).json({
-    message: err.message
+exports.getUser = function(req, res) {
+	console.log(req)
+	User.findOne(
+    {
+      '_id': req.params.id
+    }, function(err, user) {
+      if (err) {
+        return res.status(500).json({
+          message: err.message
+        });
+      }
+      if (!user) {
+      	res.send({
+					status: "error",
+					error: "User doesn't exist"
+				});
+      } else {
+        //found user. Return
+        return res.json(user);
+      }
+  });
+};
+
+/*
+exports.updateUser = function(req, res) {
+	User.findOne(
+    {
+      '_id': req
+    }, function(err, user) {
+      if (err) {
+        return res.status(500).json({
+          message: err.message
+        });
+      }
+      if (!user) {
+      } else {
+        //found user. Return
+        return res.json(user.generateJwt());
+      }
   });
 }
+
+exports.getFrineds = function(req, res) {
+	User.findOne(
+    {
+      '_id': req
+    }, function(err, user) {
+      if (err) {
+        return res.status(500).json({
+          message: err.message
+        });
+      }
+      if (!user) {
+      } else {
+        //found user. Return
+        return res.json(user.generateJwt());
+      }
+  });
+}
+*/
