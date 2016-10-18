0f999d7283bad55a8b12d11e259e22733b463781
Bohan Huang <weepineapple@gmail.com>
2016-10-18 22:59:05 +0800

    initialise combat model

---
+++ models/combat.js
@@ -0,0 +1,27 @@
+'use strict';
+var config = require('../config/config');
+var mongoose = require('mongoose');
+var User = require('../models/user');
+
+// Combat Schema
+var CombatSchema = new mongoose.Schema({
+  timestamp: Date,
+  fromUser: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+  },
+  toUser: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+  },
+  winner: Number,
+  moves: [{
+      skillId; Number,
+      fromUser: Number,
+      toUser: Number,
+      damage: Number,
+      healing: Number
+  }]
+});
+
+module.exports = mongoose.model('Combat', CombatSchema);
