8813ed80d1895822f61285b0553fed5ca8c9e3fc
Bohan Huang <weepineapple@gmail.com>
2016-10-19 15:11:38 +0800

    add create new skill method

--- controllers/skillController.js
+++ controllers/skillController.js
@@ -2,3 +2,17 @@ var jwt = require('jsonwebtoken');
 var config = require('../config/config');
 var mongoose = require('mongoose');
 var Skill = mongoose.model('Skill');
+
+exports.createSkill = function(req, res) {
+  var newSkill = new Skill(req.body.skill);
+  var promise = newSkill.save()
+  promise
+  .then(function(skill) {
+    return res.json(skill);
+  })
+  .catch(function(error) {
+    return res.status(500).json({
+      message: error.message
+    });
+  });
+}
