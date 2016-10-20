df00c290d8a081210cd0c9cc7fbd4be0d28e8641
Bohan Huang <weepineapple@gmail.com>
2016-10-20 16:25:19 +0800

    check Facebook friends again database users and add levels to returned objects

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,35 +6,35 @@ var graph = require('fbgraph');
 
 exports.getUser = function(req, res) {
 	User.findOne(
-    {
-      'facebookId': req.params.id
-    }, function(err, user) {
-      if (err) {
-        return res.status(500).json({
-          message: err.message
-        });
-      }
-      if (!user) {
-      	res.send({
+		{
+			'facebookId': req.params.id
+		}, function(err, user) {
+			if (err) {
+				return res.status(500).json({
+					message: err.message
+				});
+			}
+			if (!user) {
+				res.send({
 					status: "error",
 					error: "User doesn't exist"
 				});
-      } else {
-        //found user. Return
-        return res.json(user);
-      }
-  });
+			} else {
+				//found user. Return
+				return res.json(user);
+			}
+	});
 };
 
 exports.updateUser = function(req, res) {
 	var query = { '_id': req.user._id };
 	User.findOneAndUpdate(query, req.body.newData, { upsert:true }, function(err, user) {
-	    if (err) {
-	    	return res.status(500).json({
-         	message: err.message
-        })
-	    }
-	    res.send({
+			if (err) {
+				return res.status(500).json({
+				 	message: err.message
+				})
+			}
+			res.send({
 				status: "ok",
 			});
 	});
@@ -42,36 +42,62 @@ exports.updateUser = function(req, res) {
 
 exports.getFriends = function(req, res) {
 	User.findOne(
-    {
-      '_id': req.user._id
-    }, function(err, user) {
-      if (err) {
-        return res.status(500).json({
-          message: err.message
-        });
-      }
-      if (!user) {
-      	res.send({
+		{
+			'_id': req.user._id
+		}, function(err, user) {
+			if (err) {
+				return res.status(500).json({
+					message: err.message
+				});
+			}
+			if (!user) {
+				res.send({
 					status: "error",
 					error: "User doesn't exist"
 				});
-      } else {
-      	graph.setAccessToken(user.facebookToken);
-        graph.batch([
-				  {
-				    method: "GET",
-				    relative_url: "me/friends"
-				  }
+			} else {
+				graph.setAccessToken(user.facebookToken);
+				graph.batch([
+					{
+						method: "GET",
+						relative_url: "me/friends"
+					}
 				], function(err, graphRes) {
-				  if (err) {
-        		return res.status(500).json({
-          		message: err.message
-        		})
-        	}
-        	friends = JSON.parse(graphRes[0].body).data;
-        	return res.json(friends);
+					if (err) {
+						return res.status(500).json({
+							message: err.message
+						})
+					}
+					friends = JSON.parse(graphRes[0].body).data;
+					friends.push({"name": 'JoJo', id: '1843175555914388'});
+					console.log(friends);
+					var query = User.find({}).select('facebookId level');
+					query.exec(function (err, users) {
+						if (!err){ 
+							var friendExist = false;
+							for (var i = friends.length - 1; i >= 0; i--) {
+								friendExist = false;
+								for (var j = users.length - 1; j >= 0; j--) {
+									if (users[j].facebookId == friends[i].id) {
+										friendExist = true;
+										friends[i].level = users[j].level;
+										break;
+									}
+								};
+								if (!friendExist) {
+									friends.splice(i, 1);
+									console.log(friends);
+								}
+							};
+							return res.json(friends);
+						} else {
+							return res.status(500).json({
+								message: err.message
+							});
+						}
+					});
 				});
-      }
-  	}
-  );
+			}
+		}
+	);
 };
